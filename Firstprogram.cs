e90fd2a9930b322284833f1a178b53a461a51d48
Calculator <pranalikante21@gmail.com>
2022-04-22 12:40:01 +0530

    first commit

---
+++ Firstprogram.cs
@@ -0,0 +1,28 @@
+using System;
+namespace Operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, w, x, y, z;
+            Console.WriteLine("*****Arithmatic Program*****");
+            Console.Write("enter the 1st number:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter the 2nd number:");
+            b = Convert.ToDouble(Console.ReadLine());
+            w = a + b;
+            x = a - b;
+            y = a * b;
+            z = a / b;
+
+            Console.WriteLine("addition: " + w);
+            Console.WriteLine("subtraction: " + x);
+            Console.WriteLine("Multiplication: " + y);
+            Console.WriteLine("Divison: " + z);
+
+            Console.ReadLine();
+
+        }
+    }
+}
